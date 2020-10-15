191f12b6c90db80c3c5696f39a271b15478a5662
GeoAbin <72880067+GeoAbin@users.noreply.github.com>
2020-10-15 14:43:03 +0530

    C1

--- src/app/list-resto/list-resto.component.ts
+++ src/app/list-resto/list-resto.component.ts
@@ -13,10 +13,9 @@ export class ListRestoComponent implements OnInit {
 
   collection={}
   ngOnInit(): void {
-
-
 this.resto.getList().subscribe((result)=>{
   console.warn(result)
+  this.collection=result
 })
 }
 
